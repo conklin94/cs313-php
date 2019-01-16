6955aad233489f79c7a948196bdde464948efb40
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-15 21:38:16 -0700

    Added to header

--- web/header.php
+++ web/header.php
@@ -9,7 +9,7 @@
       <a href=index.php>
         <img id="home" src="https://image.flaticon.com/icons/svg/25/25694.svg">
       </a>
-      <h1>Seth Conklin's super cool website</h1>
+      <h1 id="headerTitle">My super cool website</h1>
     </div>
     <?php
       $x = 17;
