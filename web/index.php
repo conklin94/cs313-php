5a51afe8805464dbcfd0efd512afdfaaa654af5c
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-15 18:53:02 -0700

    Added a picture

--- web/index.php
+++ web/index.php
@@ -6,6 +6,7 @@
   </head>
   <body>
     <h1>Home Page</h1>
+    <img src="https://static.comicvine.com/uploads/original/11130/111300799/5440136-4977858128-batma.jpg" alt="Batman">
     <?php
 
       $x = 17;
