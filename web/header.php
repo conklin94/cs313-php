c825864cfa01d4e5bdc253858cfa199e12ce0c5a
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-15 21:27:03 -0700

    Made changes to the header

--- web/header.php
+++ web/header.php
@@ -7,7 +7,7 @@
   <body>
     <div class="header">
       <a href=index.php>
-        <img src="https://image.flaticon.com/icons/svg/25/25694.svg">
+        <img id="home" src="https://image.flaticon.com/icons/svg/25/25694.svg">
       </a>
       <h1>Seth Conklin's super cool website</h1>
     </div>
