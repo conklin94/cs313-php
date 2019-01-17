90250352993a227b452b8495531580452b0d2dd2
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-17 15:49:10 -0700

    Changed user.php

--- web/teach02/user.php
+++ web/teach02/user.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-if($_GET['user'] == 'admin') {
+if(htmlspecialchars($_GET['user'] == 'admin')) {
   $_SESSION["user"] = "admin";
-} elseif($_GET['user'] == 'tester') {
+} elseif(htmlspecialchars($_GET['user'] == 'tester')) {
   $_SESSION["user"] = "tester";
 } else {
   $_SESSION["user"] = "none";
