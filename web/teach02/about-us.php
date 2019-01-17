f1f3a007c357adc5bf3380b2a3f07e0907bfa219
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-17 12:03:26 -0700

    Added teach02

---
+++ web/teach02/about-us.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <p>This company takes beautiful aerial photos with a drone. We work with weddings, parties, and any event you may want to remember!</p>
+
+</body>
+</html>
---
+++ web/teach02/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>The Kelvan: something to do with pictures</h1>
+<?php
+$page = basename($_SERVER['PHP_SELF']);
+if ($page == 'about-us.php') {
+    echo "<div>
+    <a href= 'about-us.php' style='font-weight:bold;'>About Us</a>
+    <a href= 'home.php'>Home</a>
+    <a href= 'login.php'>Login</a>
+    </div>";
+}
+else if ($page == 'home.php') {
+    echo "<div>
+    <a href= 'about-us.php'>About Us</a>
+    <a href= 'home.php' style='font-weight:bold;'>Home</a>
+    <a href= 'login.php'>Login</a>
+    </div>";
+}
+else if ($page == 'TA1login.php') {
+    echo "<div>
+    <a href= 'about-us.php'>About Us</a>
+    <a href= 'home.php'>Home</a>
+    <a href= 'login.php' style='font-weight:bold;'>Login</a>
+    </div>";
+}
+?>
+</body>
+</html>
