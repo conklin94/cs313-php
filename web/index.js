a3ed7a4ee8b76736f478034787bb5ae0ee8f3879
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-16 17:44:08 -0700

    Fixed errors in javascript

--- web/index.js
+++ web/index.js
@@ -1,11 +1,9 @@
 function showBatman() {
   document.getElementById("batmanText").style.display = "block";
   document.getElementById("spidermanText").style.display = "none";
-  document.getElementById("hideButton").style.display = "block";
 }
 
 function showSpiderman() {
   document.getElementById("spidermanText").style.display = "block";
   document.getElementById("batmanText").style.display = "none";
-  document.getElementById("hideButton").style.display = "block";
 }
