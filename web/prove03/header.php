7606b6ad33c54e94d9888b510905a63a33335236
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-22 16:23:06 -0700

    Added to style

--- web/prove03/header.php
+++ web/prove03/header.php
@@ -4,14 +4,14 @@
     <title>Header</title>
     <link rel="stylesheet" href="styles.css">
   </head>
-  <body id="header">
-    <span>
+  <body>
+    <div id="header">
       <h1>The Store of Useless Treasures</h1>
       <tr>
         <th><a href="browse.php">Home</a></th>
         <th><a href="view_cart.php">View Cart</a></th>
         <th><a href="checkout.php">Checkout</a></th>
       </tr>
-    </span>
+    </div>
   </body>
 </html>
