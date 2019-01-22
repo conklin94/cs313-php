3ca3410bcce3aab39e6c0160050477fc3d3a62dd
Seth Conklin <sethconklin@Seths-MacBook-Pro.local>
2019-01-22 10:36:00 -0700

    Changed radio values

--- web/teach03/form.php
+++ web/teach03/form.php
@@ -7,10 +7,10 @@
       Name: <input type="text" name="name"><br>
       Email: <input type="text" name="email"><br>
       Major:<br>
-      <input type="radio" name="major" value="cs"> Computer Science<br>
-      <input type="radio" name="major" value="wdd"> Web Design and Development<br>
-      <input type="radio" name="major" value="cit"> Computer Information Technology<br>
-      <input type="radio" name="major" value="ce"> Computer Engineering<br>
+      <input type="radio" name="major" value="Computer Science"> Computer Science<br>
+      <input type="radio" name="major" value="Web Design and Development"> Web Design and Development<br>
+      <input type="radio" name="major" value="Computer Information Technology"> Computer Information Technology<br>
+      <input type="radio" name="major" value="Computer Engineering"> Computer Engineering<br>
       Comments:<br>
       <textarea name="comments" rows="4" cols="50"></textarea><br>
       <input type="submit">
