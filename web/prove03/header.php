1bbe0f1cc34c02786910f584f318e0b5f09fc757
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-22 16:43:32 -0700

    Changed the header to show current page

--- web/prove03/header.php
+++ web/prove03/header.php
@@ -8,9 +8,24 @@
     <div id="header">
       <h1>Our Random Stuff That Could Be Yours</h1>
       <tr>
-        <th><a href="browse.php">Home</a></th>
-        <th><a href="view_cart.php">View Cart</a></th>
-        <th><a href="checkout.php">Checkout</a></th>
+        <?php
+          $current_page = $_SERVER['REQUEST_URI'];
+          $browse = "";
+          $view_cart = "";
+          $checkout = "";
+          if ($current_page == "/prove03/browse.php") {
+            $browse = " style='text-decoration:bold;' ";
+          }
+          elseif ($current_page == "/prove03/.php") {
+            $view_cart = " style='text-decoration:bold;' ";
+          }
+          elseif ($current_page == "/prove03/browse.php") {
+            $checkout = " style='text-decoration:bold;' ";
+          }
+          echo "<th><a href='browse.php'" . $browse .">Home</a></th>";
+          echo "<th><a href='view_cart.php'" . $view_cart . ">View Cart</a></th>";
+          echo "<th><a href='checkout.php'" . $checkout . ">Checkout</a></th>";
+        ?>
       </tr>
     </div>
   </body>
