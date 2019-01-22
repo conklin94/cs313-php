651f7111547035c300f96feb3a58d3c244837c0b
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-22 11:56:09 -0700

    Added safety

--- web/teach03/results.php
+++ web/teach03/results.php
@@ -1,14 +1,14 @@
 <?php
-echo "name: " . $_POST["name"] . "<br>";
-echo "email: <a href='mailto:" . $_POST["email"] . "'>" . $_POST["email"] . "</a><br>";
-echo "major: " . $_POST["major"] . "<br>";
-echo "comments: " . $_POST["comments"] . "<br>";
-echo "You have visited:<br>";
-$country = array("NA" => "North America", "SA" => "South America",
-"EU" => "Europe", "AS" => "Asia", "AU" => "Australia", "AF" => "Africa",
-"AN" => "Antartica");
-foreach($_POST["countries"] as $value)
-{
-  echo $country[$value] . "<br>";
-}
+  echo "name: " . htmlspecialchars($_POST["name"]) . "<br>";
+  echo "email: <a href='mailto:" . htmlspecialchars($_POST["email"]) . "'>" .
+  htmlspecialchars($_POST["email"]) . "</a><br>";
+  echo "major: " . htmlspecialchars($_POST["major"]) . "<br>";
+  echo "You have visited:<br>";
+  $country = array("NA" => "North America", "SA" => "South America",
+  "EU" => "Europe", "AS" => "Asia", "AU" => "Australia", "AF" => "Africa",
+  "AN" => "Antartica");
+  foreach($_POST["countries"] as $value)
+  {
+    echo $country[htmlspecialchars($value)] . "<br>";
+  }
  ?>
