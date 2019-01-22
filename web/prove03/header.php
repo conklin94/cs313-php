8bf78d8e844d50b35f26bea265620239de48d4c2
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-22 16:45:38 -0700

    header

--- web/prove03/header.php
+++ web/prove03/header.php
@@ -22,9 +22,10 @@
           elseif ($current_page == "/prove03/browse.php") {
             $checkout = " style='text-decoration:bold;' ";
           }
-          echo "<th><a href='browse.php'" . $browse .">Home</a></th>";
-          echo "<th><a href='view_cart.php'" . $view_cart . ">View Cart</a></th>";
-          echo "<th><a href='checkout.php'" . $checkout . ">Checkout</a></th>";
+          echo "Current Page: $current_page <br>";
+          echo "<th><a href='browse.php'" . $browse .">  Home  </a></th>";
+          echo "<th><a href='view_cart.php'" . $view_cart . ">  View Cart  </a></th>";
+          echo "<th><a href='checkout.php'" . $checkout . ">  Checkout  </a></th>";
         ?>
       </tr>
     </div>
