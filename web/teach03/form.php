8750293f5c13722bce6652a947e658b81bc97da4
Seth Conklin <sethconklin@Seths-MacBook-Pro.local>
2019-01-22 10:23:22 -0700

    Added teach02 stuff

---
+++ web/teach03/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <form action="results.php" method="post">
+      Name: <input type="text" name="name"><br>
+      Email: <input type="text" name="email"><br>
+      Major<br>
+      <input type="radio" name="major" value="cs"> Computer Science<br>
+      <input type="radio" name="major" value="wdd"> Web Design and Development<br>
+      <input type="radio" name="major" value="cit"> Computer Information Technology<br>
+      <input type="radio" name="major" value="ce"> Computer Engineering<br>
+      Comments<br>
+      <textarea rows="4" cols="50"></textarea>
+      <input type="submit">
+    </form>
+  </body>
+</html>
