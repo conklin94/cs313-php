fa09f0b376895b498c601cc3fe1033479a9cbb27
Seth Conklin <sethconklin@Seths-MacBook-Pro.local>
2019-01-22 10:34:15 -0700

    Added more to results

--- web/teach03/results.php
+++ web/teach03/results.php
@@ -1,4 +1,6 @@
 <?php
 echo "name: " . $_POST["name"] . "<br>";
 echo "email: <a href='mailto:" . $_POST["email"] . "'>" . $_POST["email"] . "</a><br>";
+echo "major: " . $_POST["major"] . "<br>";
+echo "comments: " . $_POST["comments"] . "<br>";
  ?>
