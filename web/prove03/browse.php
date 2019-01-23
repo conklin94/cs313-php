98152266a752cbbcfef299c6a23de771dc110d6b
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-22 17:36:38 -0700

    Made items part of php

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -7,26 +7,18 @@
   <body>
     <?php
       include 'header.php';
+      $names = array("Mini Piano", "Kaleidoscope", "Batman Toothbrosh",
+      "Novelty Socks")
+      $images = array("https://cdn.shopify.com/s/files/1/2256/4721/products/2522R_SIDE_1600x.png?v=1547238423",
+      "https://cdn.shopify.com/s/files/1/0130/8502/products/2315WJPG_1_2000x.jpg?v=1485396369",
+      "https://www.fireflytoothbrush.com/wp-content/uploads/2016/10/92767_BTM_TurboStand_brush.jpg",
+      "https://cdn.shopify.com/s/files/1/0569/1997/products/ozone_socks_WC1247-15_endorphic_palm_580x@2x.jpg?v=1547573919")
+      for ($x = 0, $x <=4, $x++) {
+        echo "<div class='item'>";
+        echo "<h2>" . $names[$x] . "</h2>";
+        echo "<img src='" . $images[$x] . "' alt='" . $names[$x] . "'>";
+        echo "</div>";
+      }
      ?>
-     <div class="item">
-       <h2>Mini Piano</h2>
-       <img src="https://cdn.shopify.com/s/files/1/2256/4721/products/2522R_SIDE_1600x.png?v=1547238423"
-       alt="mini-piano">
-     </div>
-     <div class="item">
-       <h2>Kaleidoscope</h2>
-       <img src="https://cdn.shopify.com/s/files/1/0130/8502/products/2315WJPG_1_2000x.jpg?v=1485396369"
-       alt="kaleidoscope">
-     </div>
-     <div class="item">
-       <h2>Batman Toothbrush</h2>
-       <img src="https://www.fireflytoothbrush.com/wp-content/uploads/2016/10/92767_BTM_TurboStand_brush.jpg"
-       alt="batman-toothbrush">
-     </div>
-     <div class="item">
-       <h2>Novelty Socks</h2>
-       <img src="https://cdn.shopify.com/s/files/1/0569/1997/products/ozone_socks_WC1247-15_endorphic_palm_580x@2x.jpg?v=1547573919"
-       alt="novelty-socks">
-     </div>
   </body>
 </html>
