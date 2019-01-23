70d1298d7b6973417a4ce786a07cd3c3f92dd01f
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-22 17:05:57 -0700

    first item

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -2,11 +2,16 @@
 <html>
   <head>
     <title>Browsing Page</title>
+    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
     <?php
       include 'header.php';
      ?>
-     <h1>This is the browse page</h1>
+     <div class="item">
+       <h2>Mini Piano</h2>
+       <img src="https://cdn.shopify.com/s/files/1/2256/4721/products/2522R_SIDE_1600x.png?v=1547238423"
+       alt="mini-piano">
+     </div>
   </body>
 </html>
