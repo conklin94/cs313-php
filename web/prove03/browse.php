c02c88604caee91b0ebb761137068a3d2295e057
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-22 17:39:32 -0700

    Fixed problem

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -13,7 +13,7 @@
       "https://cdn.shopify.com/s/files/1/0130/8502/products/2315WJPG_1_2000x.jpg?v=1485396369",
       "https://www.fireflytoothbrush.com/wp-content/uploads/2016/10/92767_BTM_TurboStand_brush.jpg",
       "https://cdn.shopify.com/s/files/1/0569/1997/products/ozone_socks_WC1247-15_endorphic_palm_580x@2x.jpg?v=1547573919");
-      for ($x = 0, $x <=4, $x++) {
+      for ($x = 0, $x < 4, $x++) {
         echo "<div class='item'>";
         echo "<h2>" . $names[$x] . "</h2>";
         echo "<img src='" . $images[$x] . "' alt='" . $names[$x] . "'>";
