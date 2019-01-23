eae055f212a844d980b5598bf1d111d3176f59f0
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-22 20:40:42 -0700

    Added remove functionality to shopping cart

--- web/prove03/view_cart.php
+++ web/prove03/view_cart.php
@@ -7,15 +7,33 @@
     <title>Shopping Cart</title>
     <link rel="stylesheet" href="styles.css">
   </head>
-  <body>
+  <body id="view_cart">
     <?php
       include 'header.php';
+
+      if (isset($_POST['remove'])) {
+        $name = $_POST['remove'];
+        if (isset($_SESSION['cart']) && isset($_SESSION['cart'][$name])) {
+          if ($_SESSION['cart'][$name]['quantity'] == 1)
+          {
+            unset($_SESSION['cart'][$name);
+          }
+          else {
+            $_SESSION['cart'][$name['quantity'] -= 1;
+          }
+        }
+      }
+
       if (isset($_SESSION['cart'])) {
         echo "<h1>Your Shopping Cart</h1>";
         foreach ($_SESSION['cart'] as $key => $value) {
           echo "<h2>" . $key . "</h2>";
           echo "<h3>Price: $" . number_format($value['price'],2) .
           ", Quantity: " . $value['quantity'] . "</h3>";
+          echo "<form action='view_cart.php' method='post'>";
+          echo "  <button type='submit' name='remove' value='" . $key . "'>
+          Remove item</button>";
+          echo "  </form>";
         }
       }
       else {
