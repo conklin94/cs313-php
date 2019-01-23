be781a8b1f9ed4ea2bcbeba8545fcb3e2dd38eb3
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-01-22 20:59:14 -0700

    Added a form to checkout page

--- web/prove03/checkout.php
+++ web/prove03/checkout.php
@@ -1,13 +1,28 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <title>Checkout Page</title>
     <link rel="stylesheet" href="styles.css">
   </head>
-  <body>
+  <body id="checkout">
     <?php
       include 'header.php';
+
+      if (isset($_SESSION['cart'])) {
+        echo "  <form action='confirmation.php' method='post'>";
+        echo "Name: <input type='text' name='name'><br>";
+        echo "Street Address: <input type='text' name='street_address'><br>";
+        echo "City: <input type='text' name='city'><br>";
+        echo "State: <input type='text' name='state'><br>";
+        echo "Zip Code: <input type='text' name='zip_code'><br>";
+        echo "<input type='submit'>";
+      }
+      else {
+        echo "<h1>You cannot checkout because your shopping cart is empty</h1>";
+      }
      ?>
-     <h1>This is the Checkout page</h1>
   </body>
 </html>
