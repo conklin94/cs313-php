4421c0e97ce47c27b939c3476adec7880af82fa2
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-02-08 13:20:59 -0700

    Changed sizes

--- web/book_review/login.php
+++ web/book_review/login.php
@@ -19,9 +19,9 @@
     ?>
     <br>
     <form id="login" action="log_in_or_out.php" method="post">
-      <label for="username">Username</label><br>
+      <h3>Username</h3><br>
       <input type="text" name="username"><br>
-      <label for="password">Password</label><br>
+      <h3>Password</h3><br>
       <input type="password" name="password"><br>
       <button type="submit" name="login" value="True">Log in</button>
     </form>
