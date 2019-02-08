b2133beb5d390ce17d0b74ac4b8bf6f980ae53fd
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-02-08 13:23:46 -0700

    style

--- web/book_review/login.php
+++ web/book_review/login.php
@@ -19,9 +19,9 @@
     ?>
     <br>
     <form id="login" action="log_in_or_out.php" method="post">
-      <h3>Username</h3><br>
+      <label for="username">Username</label><br>
       <input type="text" name="username"><br>
-      <h3>Password</h3><br>
+      <label for="password">Password</label><br>
       <input type="password" name="password"><br>
       <button type="submit" name="login" value="True">Log in</button>
     </form>
