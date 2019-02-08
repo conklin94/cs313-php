d7bdb5035efe99742e35fa61e80c1b8d5b9bd90c
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-02-08 09:27:33 -0700

    Added logout functionality

--- web/book_review/header.php
+++ web/book_review/header.php
@@ -11,6 +11,21 @@
     <div id='header'>
       <h1>Book Reviewer</h1>
       <h3>A place to find books to read and rate books you've read</h3>
+      <?php
+        if (isset($_SESSION['logged_in'])) {
+          echo "  <form action=\"log_in_or_out.php\" method=\"post\">";
+          echo "    <button type=\"submit\" name=\"login\" value=\"False\">
+          Log out</button>";
+          echo "  </form>";
+        }
+        else {
+          echo "  <form action=\"login.php\" method=\"post\">";
+          echo "    <button type=\"submit\">Log in</button>";
+          echo "  </form>";
+        }
+      ?>
+      <img src="https://cdn2.iconfinder.com/data/icons/blue-round-amazing-icons-1/512/home-alt-512.png"
+      alt="Home Page">
     </div>
   </body>
 </html>
