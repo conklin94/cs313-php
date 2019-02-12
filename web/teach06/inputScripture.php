b92efc5e1fcd3eaea8099e0a2049a61ca7beed71
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-02-12 15:46:55 -0700

    Added scriptures to input page

--- web/teach06/inputScripture.php
+++ web/teach06/inputScripture.php
@@ -1,31 +1,53 @@
 <?php
   require 'db_access.php';
  ?>
- <!DOCTYPE html>
- <html>
- <head>
- </head>
- <body>
-   <form action="insertScripture.php" method="post">
-     Book: <input type="text" name="book"><br>
-     Chapter: <input type="text" name="chapter"><br>
-     Verse: <input type="text" name="verse"><br>
-     Content: <textarea rows="4" cols="50" name="content"></textarea><br>
-     <?php
-       $db = get_db();
-       $statement = $db->query('SELECT id, name FROM Topic');
-       while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-       {
-         $id = $row['id'];
-         $name = $row['name'];
-         echo "<input type='checkbox' name='topics[]' value='$id'>$name<br>";
-       }
-       echo "<input type='checkbox' name='topic' value='yes'>";
-       echo "<input type='text' name='new_topic'><br>";
-      ?>
-     <input type="submit">
-   </form>
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <?php
+      $db = get_db();
+      $statement = $db->query('SELECT id, book, chapter, verse FROM Scriptures');
+      while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+      {
+        $scripture_id = $row['id'];
+        $book = $row['book'];
+        $chapter = $row['chapter'];
+        $verse = $row['verse'];
+        echo "<h1>$book $chapter:$verse</h1>";
+        $stmt = $db->prepare('SELECT (SELECT name FROM Topic WHERE id = st.topic_id) AS name
+                              FROM Scripture_Topic st
+                              WHERE scripture_id = :scripture_id');
+        $stmt->execute(array(':scripture_id' => $scripture_id));
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo "<ul>";
+        foreach ($rows as $row2) {
+          $name = $row2['name'];
+          echo "<li>$name</li>";
+        }
+        echo "</ul>";
+      }
+  ?>
+    <form action="insertScripture.php" method="post">
+      Book: <input type="text" name="book"><br>
+      Chapter: <input type="text" name="chapter"><br>
+      Verse: <input type="text" name="verse"><br>
+      Content: <textarea rows="4" cols="50" name="content"></textarea><br>
+      <?php
+        $statement = $db->query('SELECT id, name FROM Topic');
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
+          $id = $row['id'];
+          $name = $row['name'];
+          echo "<input type='checkbox' name='topics[]' value='$id'>$name<br>";
+        }
+        echo "<input type='checkbox' name='topic' value='yes'>";
+        echo "<input type='text' name='new_topic'><br>";
+       ?>
+      <input type="submit">
+    </form>
 
 
- </body>
- </html>
+  </body>
+</html>
--- web/teach06/insertScripture.php
+++ web/teach06/insertScripture.php
@@ -1,11 +1,11 @@
 <?php
   require 'db_access.php';
   $db = get_db();
-  $book = $_POST['book'];
-  $chapter = $_POST['chapter'];
-  $verse = $_POST['verse'];
-  $content = $_POST['content'];
-  $topics = $_POST['topics'];
+  $book = htmlspecialchars($_POST['book']);
+  $chapter = htmlspecialchars($_POST['chapter']);
+  $verse = htmlspecialchars($_POST['verse']);
+  $content = htmlspecialchars($_POST['content']);
+  $topics = htmlspecialchars($_POST['topics']);
 
   try {
     $stmt = $db->prepare('INSERT INTO Scriptures (book, chapter, verse, content)
@@ -18,9 +18,16 @@
       $newId = $db->lastInsertId('scriptures_id_seq');
       $stmt->execute(array(':scripture_id' => $newId, ':topic_id' => $topic));
     }
+    if ($_POST['topic'] == 'yes') {
+      $topic = htmlspecialchars($_POST['new_topic']);
+      $stmt = $db->prepare('INSERT INTO Scripture_Topic (scripture_id, topic_id)
+                            VALUES (:scripture_id, :topic_id)');
+      $newId = $db->lastInsertId('scriptures_id_seq');
+      $stmt->execute(array(':scripture_id' => $newId, ':topic_id' => $topic));
+    }
   }
   catch (exception $e) {
     echo "Fail";
   }
-  header('Location: scriptureList.php');
+  header('Location: inputScripture.php');
  ?>
