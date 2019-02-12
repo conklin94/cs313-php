62d60377356e26e3ce4475cd0727be4ad1923d02
Seth Conklin <sethconklin@Seths-MacBook-Pro.local>
2019-02-12 10:32:02 -0700

    Scripture stuff

---
+++ web/teach06/inputScripture.php
@@ -0,0 +1,29 @@
+<?php
+  require 'db_access.php';
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ </head>
+ <body>
+   <form action="insertScripture.php" method="post">
+     Book: <input type="text" name="book"><br>
+     Chapter: <input type="text" name="chapter"><br>
+     Verse: <input type="text" name="verse"><br>
+     Content: <textarea rows="4" cols="50" name="content"></textarea>
+     <?php
+       $db = get_db();
+       $statement = $db->query('SELECT id, name FROM Topic');
+       while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+       {
+         $id = $row['id'];
+         $name = $row['name'];
+         echo "<input type='checkbox' name='topics[]' value='$id'>$name<br/>";
+       }
+      ?>
+     <input type="submit">
+   </form>
+
+
+ </body>
+ </html>
