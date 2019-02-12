c324789481fdc6dc2ffa46a85bd628f50960238f
Seth Conklin <sethconklin@Seths-MacBook-Pro.local>
2019-02-12 11:15:56 -0700

    scripture topic

--- web/teach06/scriptureList.php
+++ web/teach06/scriptureList.php
@@ -11,20 +11,20 @@
        $statement = $db->query('SELECT id, book, chapter, verse FROM Scriptures');
        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
        {
-         $id = $row['id'];
+         $scripture_id = $row['id'];
          $book = $row['book'];
          $chapter = $row['chapter'];
          $verse = $row['verse'];
          echo "<h1>$book $chapter:$verse</h1>";
-         #$stmt = $db->prepare('SELECT name FROM Topic WHERE scripture_id = :id');
-         #$stmt->execute(array(':id' => $id);
-         #$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-         #echo "<ul>";
-         #foreach ($rows as $row2) {
-         #  $name = $row2['name'];
-         #  echo "<li>$name</li>";
-         #}
-         #echo "</ul>";
+         $stmt = $db->prepare('SELECT name FROM Scripture_Topic WHERE scripture_id = :scripture_id');
+         $stmt->execute(array(':scripture_id' => $scripture_id);
+         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+         echo "<ul>";
+         foreach ($rows as $row2) {
+           $name = $row2['name'];
+           echo "<li>$name</li>";
+         }
+         echo "</ul>";
        }
   ?>
 
