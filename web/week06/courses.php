7c687f062f9d8575887878b79870e62420b1c289
Seth Conklin <sethconklin@Seths-MacBook-Pro.local>
2019-02-14 10:20:57 -0700

    Added week 6 notes

---
+++ web/week06/courses.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Courses</title>
+  </head>
+  <body>
+    <h1>Notes App</h1>
+
+    <h2>Courses</h2>
+
+    <ul>
+       <li>CS xx text</li>
+       <li>Something</li>
+    </ul>
+  </body>
+</html>
