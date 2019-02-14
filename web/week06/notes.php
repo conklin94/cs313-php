737bff0a3bfdc2e3a4f4c3228beed02f80d10d2f
Seth Conklin <sethconklin@Seths-MacBook-Pro.local>
2019-02-14 10:56:49 -0700

    course code

--- web/week06/notes.php
+++ web/week06/notes.php
@@ -3,6 +3,12 @@ require_once('dbconnect.php');
 $db = get_db();
 
 $course_id = $_GET['course_id'];
+
+$query = 'SELECT id, name, course_code FROM course WHERE id = :course_id';
+$statement = $db->prepare($query);
+$statement->execute(array(':course_id' => $course_id));
+$course = $statement->fetch(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -12,9 +18,10 @@ $course_id = $_GET['course_id'];
   </head>
   <body>
 <?php
-echo "<h1> Notes for $course_id</h1>"; 
+$course_name = $course['name'];
+$course_code = $course['course_code'];
+echo "<h1> Notes for $course_code - $course_name</h1>"; 
 ?>
-    <h1>Notes for XXXX - XXXXXXX</h1>
 
   </body>
 </html>
