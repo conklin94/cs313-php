efd47ee97e608f95b9420c92ea8657fbf8d79ba8
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-02-14 19:01:04 -0700

    Add new user

--- web/book_review/create_new_user.php
+++ web/book_review/create_new_user.php
@@ -19,15 +19,12 @@
       <label for="last_name">Last Name:</label><br>
       <input type="text" name="last_name"><br>
       <label for="username">Username:</label><br>
-      <input type="text" name="last_name"><br>
+      <input type="text" name="username"><br>
       <label for="password">Password:</label><br>
       <input type="password" name="password"><br>
       <label for="password2">Re-enter Password:</label><br>
       <input type="password" name="password2"><br>
       <button type="submit" name="create_user">Create</button>
-    </form>
-    <a class="link" href='create_new_user.php'>Create New User</a>
-
     </form>
 
   </body>
