6bc4acc54a98ffb70327174780c955eebad7e793
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-02-14 17:42:55 -0700

    Class work

--- web/week06/insert_note.php
+++ web/week06/insert_note.php
@@ -4,4 +4,11 @@ $date = htmlspecialchars($_POST["date"]);
 $content = htmlspecialchars($_POST["content"]);
 
 echo "Trying to insert: courseid: $course_id, date: $date, content: $content";
+$query = 'INSERT INTO note(date, content, course_id) VALUES
+          (:date, :content, :course_id)';
+$statement = $db->prepare($query);
+$statement->execute(array(':course_id' => $course_id, ':date' => $date,
+                          ':content' => $content));
+$course = $statement->fetch(PDO::FETCH_ASSOC);
+
 ?>
