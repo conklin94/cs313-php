47e08c8b0cad65c30493b8050b192a6438bc1c26
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-02-19 15:52:16 -0700

    Testing

--- web/book_review/update.php
+++ web/book_review/update.php
@@ -4,13 +4,16 @@ $db = get_db();
 
 try
 {
+  echo "Part 1 ";
   $stmt = $db->prepare('SELECT username, password FROM reader');
   $stmt->execute();
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+  echo " Part 2 ";
   foreach ($rows as $row) {
     $username = $row['username'];
     $password = $row['password'];
+    echo " Part 3 ";
     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
     $stmt = $db->prepare('UPDATE reader SET password = :password
                           WHERE username = :username');
