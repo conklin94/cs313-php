fe6dc1c3947d679ac0dc49a06c84a6d820c6b90f
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-02-21 08:50:18 -0700

    password

--- web/book_review/script.js
+++ web/book_review/script.js
@@ -1,6 +1,6 @@
 function validatePassword() {
-  var password1 = document.getElementsById("pass1").value;
-  var password2 = document.getElementsById("pass2").value;
+  var password1 = document.getElementById("pass1").value;
+  var password2 = document.getElementById("pass2").value;
   var text = document.getElementById("password_text");
   if (password1 != password2) {
     text.style.display = "inline";
