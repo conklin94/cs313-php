7dd2416657372224e415e2ed5c2fb0214c286c7f
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-02-21 08:42:56 -0700

    Test

--- web/book_review/script.js
+++ web/book_review/script.js
@@ -1,10 +1,11 @@
 function validatePassword() {
-  var password1 = document.getElementsByName("password1").value;
-  var password2 = document.getElementsByName("password2").value;
-  var text = document.getElementById("password_text");
-  if (password1 != password2) {
-    text.style.display = "inline";
-  } else {
-    text.style.display = "none";
+  //var password1 = document.getElementsByName("password1").value;
+  //var password2 = document.getElementsByName("password2").value;
+  //var text = document.getElementById("password_text");
+  //if (password1 != password2) {
+  //  text.style.display = "inline";
+  //} else {
+  //  text.style.display = "none";
+  alert("IT was called at least");
   }
 }
